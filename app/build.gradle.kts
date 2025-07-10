8e1d3fd29aa8b33acab3be157f97e8e08ff05b9c
NiccoRoss <niccomedina62@gmail.com>
2025-07-09 22:30:00 -0500

    Actualizo diseño, agrego menú y WebView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.12.0")
+
+
 
 }
--- app/src/main/java/com/example/copycrunchyroll/webview.kt
+++ app/src/main/java/com/example/copycrunchyroll/webview.kt
@@ -1,13 +1,17 @@
 package com.example.copycrunchyroll
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class webview : AppCompatActivity() {
 
+    private lateinit var progressBar: ProgressBar
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,8 @@ class webview : AppCompatActivity() {
         // Toolbar
         val toolbar: Toolbar = findViewById(R.id.web_toolbar)
         setSupportActionBar(toolbar)
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Título dinámico
@@ -27,11 +33,20 @@ class webview : AppCompatActivity() {
         webView = findViewById(R.id.webView)
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                progressBar.visibility = View.GONE
+            }
+
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view?.loadUrl(url ?: "")
                 return true
             }
         }
+
 
         val url = intent.getStringExtra("url")
             ?: "https://www.crunchyroll.com/es-es/tos/"
